d6f10f5ecdcfd14dadd7d3e79dd9ee0309535e63
Zyon Savery <zsavery998@icloud.com>
2023-11-07 01:04:03 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to my api!"}
+
+
+@app.get("/post")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(newpost: Post):
+    print(newpost.title)
+    return {"data": "newpost"}
+
+
+# title str, content str
